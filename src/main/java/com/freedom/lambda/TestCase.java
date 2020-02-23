8b6904bbdaf83f6ab14b900d6f5d070faba136e5
free <oncwnuM3kr4gzkR8gi52AQSjKbTU@git.weixin.qq.com>
2020-02-23 20:21:04 +0800

    lambda reduce| max| min

--- src/main/java/com/freedom/lambda/TestCase.java
+++ src/main/java/com/freedom/lambda/TestCase.java
@@ -1,8 +1,6 @@
 package com.freedom.lambda;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -36,13 +34,39 @@ public class TestCase {
         students.add(new Student("路飞", 22, 175));
         students.add(new Student("红发", 40, 180));
         students.add(new Student("白胡子", 50, 185));
-        List<Student> newstudentList = Stream.of(students, Arrays.asList(new Student("艾斯", 25, 183),
-            new Student("雷利", 48, 176))).flatMap(students1 -> students1.stream()).collect(Collectors.toList());
+        List<Student> newstudentList = Stream.of(students, Arrays.asList(
+                new Student("艾斯", 25, 183),
+                new Student("雷利", 48, 176))).
+                flatMap(students1 -> students1.stream()).collect(Collectors.toList());
         System.out.println("flatMap:" +newstudentList);
 
 
 
         //max和min。经常在集合中求最大或最小值，使用流就很方便。及早求值。
+        Optional<Student> max = students.stream().max(Comparator.comparing(stu -> stu.getAge()));
+
+        Optional<Student> min = students.stream().min(Comparator.comparing(stu -> stu.getAge()));
+        //判断是否有值
+        if (max.isPresent()) {
+            System.out.println("max age:" + max.get());
+        }
+        if (min.isPresent()){
+            System.out.println("min age:" + min.get());
+        }
+
+
+        //count 统计功能，一般都是结合filter使用，因为先筛选出我们需要的再统计即可。及早求值。
+
+        long count = students.stream().filter(s1 -> s1.getAge() < 45).count();
+        System.out.println("年龄小于45岁的人数是 ： " + count);
+
+        //reduce 操作 可以实现从一组值中生成一个值。  count、min和max方法，因为常用而被纳入标准库中。
+        // 事实上，这些方法都是reduce操作
+        Integer reduce = Stream.of(1,2,3,4).reduce(0,(acc,x) -> acc+x);//累加器
+        Integer reduce2 = Stream.of(1,2,3,4).reduce(1,(acc,x) -> acc*x);//累乘器
+
+        System.out.println(reduce);
+
 
     }
 }
